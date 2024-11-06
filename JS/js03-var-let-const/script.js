cbc27a6d234478e04a684e76b825c58f9a1b7379
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-07 00:21:07 +0100

    js3

---
+++ JS/js03-var-let-const/script.js
@@ -0,0 +1,20 @@
+//let, var and const
+
+// var
+var name1 = "John Doe";
+console.log(typeof name1);
+let name2 = 1200;
+console.log(typeof name2);
+
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2);
+
+let not = 40;
+let situation = not >= 50;
+console.log(situation);
+console.log(typeof situation);
+
+let jahr;
+console.log(jahr);
+console.log(typeof jahr);
