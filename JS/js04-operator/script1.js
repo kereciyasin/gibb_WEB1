8c9d6391dabb9d7f328615fbcad0175405b710aa
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-07 10:47:22 +0100

    ubung 2

---
+++ JS/js04-operator/script1.js
@@ -0,0 +1,9 @@
+let age;
+let education = "high school";
+
+age = 20;
+if (age >= 18 && education === "high school") {
+  console.log("You are eligible to apply for the job.");
+} else {
+  console.log("You are not eligible to apply for the job.");
+}
