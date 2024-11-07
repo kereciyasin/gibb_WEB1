bd17f7206162b675e94325c2f2aa2381d73e3e0d
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-07 10:55:05 +0100

    string

---
+++ JS/js05-string/script.js
@@ -0,0 +1,16 @@
+let vorname = "Yasin";
+let nachname = "Kereci";
+let jahr = 1995;
+let stadt = "Bern";
+
+let text =
+  "Mein Name ist " +
+  vorname +
+  " " +
+  nachname +
+  " und ich bin " +
+  (2024 - jahr) +
+  " Jahre alt. Ich wohne in " +
+  stadt +
+  ".";
+console.log(text);
