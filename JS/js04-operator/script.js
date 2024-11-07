8bb02c4b29cff8b19738dc9feeef295852dc5ef4
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-07 10:10:59 +0100

    if else

---
+++ JS/js04-operator/script.js
@@ -0,0 +1,42 @@
+// let result;
+// let a = 10,
+//   b = 5,
+//   c = 30;
+
+// // Addition
+// result = a + b;
+// console.log(result); // 15
+
+// // Subtraction
+// result = a - b;
+// console.log(result); // 5
+
+// // Multiplication
+// result = a * b;
+// console.log(result); // 50
+
+// // Division
+// result = a / b;
+// console.log(result); // 2
+
+// // Modulus
+// result = a % b;
+// console.log(result); // 0
+
+// // Exponentiation
+// result = a ** b;
+// console.log(result); // 100000
+
+// // Increment
+// result = a++;
+// console.log(result); // 10
+
+// if else
+
+let isLoggedin = true;
+
+if (isLoggedin) {
+  console.log("User is logged in");
+} else {
+  console.log("User is not logged in");
+}
