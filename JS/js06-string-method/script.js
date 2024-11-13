51de8e1a331b45e72052dce975c2d1ac2cccdc81
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-13 22:58:05 +0100

    string method +

--- JS/js06-string-method/script.js
+++ JS/js06-string-method/script.js
@@ -14,4 +14,17 @@ result = kursName.substring(0, 10);
 // length property
 result = kursName.length;
 
+// replace() methodu
+result = kursName.replace("JavaScript", "Python");
+
+// trim methodu bosluklari sil
+result = kursName.trim();
+result = kursName.trimEnd();
+result = kursName.trimStart();
+
+// split() methodu stringi arraya cevirir
+result = kursName.split(" ");
+result = kursName.split(" ")[0];
+result = kursName.split(" ")[3];
+
 console.log(result);
