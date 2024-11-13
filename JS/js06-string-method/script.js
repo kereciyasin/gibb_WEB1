15890e1ac3497438be7394999dc6ef2b9f0301a1
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-13 22:45:24 +0100

    string method

---
+++ JS/js06-string-method/script.js
@@ -0,0 +1,17 @@
+let kursName = "JavaScript kursu: Modern JavaScript Kursu";
+
+let result;
+
+// toLowerCase() methodu
+result = kursName.toLowerCase();
+
+// toUpperCase() methodu
+result = kursName.toUpperCase();
+
+// substring() methodu
+result = kursName.substring(0, 10);
+
+// length property
+result = kursName.length;
+
+console.log(result);
