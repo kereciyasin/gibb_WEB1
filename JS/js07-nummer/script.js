ba91f0b97923ae490bd570f39ad0be65edb5302b
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-14 23:46:54 +0100

    nummer js

---
+++ JS/js07-nummer/script.js
@@ -0,0 +1,10 @@
+let result;
+
+result = 10; // number
+result = "10"; // string
+result = Number("10"); // number
+result = parseInt("10.6"); // number
+result = parseFloat("10.6"); // double
+
+console.log(typeof result); // number
+console.log(result); // 10
