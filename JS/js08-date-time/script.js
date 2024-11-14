04916f2c21b7e732738c45eaca0eaf2c7a0c79a5
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-15 00:11:20 +0100

    arrays

--- JS/js08-date-time/script.js
+++ JS/js08-date-time/script.js
@@ -14,8 +14,23 @@ result = heute.getTime(); // 1970 den bu yana geçen milisaniye sayısı gelir
 // Set methods
 heute.setFullYear(2023); // yılı değiştirir
 heute.setMonth(11); // ayı değiştirir
+heute.setDate(12); // günü değiştirir
 
 result = heute;
+
+// Nesne oluştururken tarih ve saat belirleme
+
+let birthday = new Date(1995, 2, 6);
+
+result = heute.getFullYear() - birthday.getFullYear();
+result = heute - birthday; // iki tarih arasındaki farkı milisaniye cinsinden verir.
+let miniSecond = heute - birthday;
+let saniye = miniSecond / 1000;
+let dakika = saniye / 60;
+let saat = dakika / 60;
+let gün = saat / 24;
+
+result = gün;
 
 console.log(result);
 console.log(typeof result);
---
+++ JS/js09-array/script.js
@@ -0,0 +1,7 @@
+let marke = ['BMW', 'Audi', 'VW', 'Mercedes', 'Opel'];
+let price = [50000, 45000, 40000, 55000, 30000];  
+let color = ['black', 'white', 'blue', 'red', 'green'];
+let year = [2018, 2019, 2020, 2021, 2022];
+
+console.log(marke);
+console.log(price[2]);  // 40000
