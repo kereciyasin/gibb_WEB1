38e34b8fb212aff9769a6cd8a9212febba57c66d
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-15 00:00:30 +0100

    set methods

--- JS/js08-date-time/script.js
+++ JS/js08-date-time/script.js
@@ -3,13 +3,19 @@ let heute = new Date(); // aktuelles Datum und Uhrzeit
 result = heute;
 result = heute.getDate(); // gün gelir
 result = heute.getDay(); // günün sırası gelir(hangi gün olduğunu verir) 0 pazardir.
-
+// Get methods
 result = heute.getFullYear(); // yıl gelir
 result = heute.getHours(); // saat gelir
 result = heute.getMinutes(); // dakika gelir
 result = heute.getSeconds(); // saniye gelir
 result = heute.getMilliseconds(); // milisaniye gelir
 result = heute.getTime(); // 1970 den bu yana geçen milisaniye sayısı gelir
+
+// Set methods
+heute.setFullYear(2023); // yılı değiştirir
+heute.setMonth(11); // ayı değiştirir
+
+result = heute;
 
 console.log(result);
 console.log(typeof result);
