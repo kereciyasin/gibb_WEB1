5b829dc8ef0433793b079d416e4da374bf522606
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-15 14:53:45 +0100

    objects

---
+++ JS/js10-objects/script.js
@@ -0,0 +1,18 @@
+let user1 = ["Yasin", "Kereci", 29];
+let user2 = ["Yasin", "Güven", 29];
+
+//dict, json
+
+let user = {
+  name: "Yasin",
+  surname: "Kereci",
+  age: 29,
+};
+
+let result;
+
+result = user.name;
+result = user.surname;
+// result = user["name"];
+
+console.log(result);
