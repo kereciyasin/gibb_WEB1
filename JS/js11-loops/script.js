63ee8ff9d32e90f97395e18f44f9689659aa479c
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-16 07:57:23 +0100

    js

--- JS/js11-loops/script.js
+++ JS/js11-loops/script.js
@@ -1,3 +1,12 @@
-for (let i = 0; i <= 10; i++) {
-  console.log(i);
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+let nummers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let summen = 0;
+for (let i = 0; i < nummers.length; i++) {
+  // console.log(nummers[i]);
+  summen += nummers[i];
 }
+console.log(summen);
