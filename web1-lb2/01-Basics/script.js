1119c8ef3ce56f795060327f0d6532b1879b6a85
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-26 15:04:05 +0100

    01_basic end

---
+++ web1-lb2/01-Basics/script.js
@@ -0,0 +1,6 @@
+const num1 = prompt("Lütfen birinci sayıyı girin:");
+const num2 = prompt("Lütfen ikinci sayıyı girin:");
+const sum = parseInt(num1) + parseInt(num2);
+console.log(
+  "Girdiğiniz " + num1 + " ve " + num2 + " sayılarının toplamı: " + sum
+);
