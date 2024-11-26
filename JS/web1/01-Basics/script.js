769a16924ee2f9f95bca63b9195d670b5b376408
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-26 14:44:36 +0100

    added

--- JS/web1/01-Basics/script.js
+++
@@ -1,4 +0,0 @@
-const num1 = prompt("Gib die erste Zahl ein");
-const num2 = prompt("Gib die zweite Zahl ein");
-const sum = parseInt(num1) + parseInt(num2);
-console.log(`Die Summe von ${num1} und ${num2} ist ${sum}`);
--- JS/web1/02-Variables/variables.js
+++
@@ -1,7 +0,0 @@
-let name = "John";
-let vorname = "Doe";
-let age = 35;
-
-console.log("Name", name);
-console.log("Vorname", vorname);
-console.log("Alter", age);
