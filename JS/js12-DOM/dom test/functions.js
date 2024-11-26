b9ce984b40eb18f4777df2c5c06c9a5fa7596ef6
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-26 13:11:19 +0100

    functions

---
+++ JS/js12-DOM/dom test/functions.js
@@ -0,0 +1,27 @@
+// function greet(name){
+//   console.log("Hallo " + name + "!" );
+// }
+// greet();
+// //
+// greet("Yasin", "Veli")
+
+// let metin = "Ben Yasin Kereci. Merhaba"
+
+// let harf = "a"
+
+// function bul(harf){
+//   toplam = 0
+//   for(i=0; i < metin.length; i++){
+//     if(metin.charAt(i)==harf){
+//         toplam = toplam + 1;
+//     }
+//   }
+//   return toplam
+// }
+// let harfSayisi = bul(harf)
+// console.log("Sonuc: " + harfSayisi);
+
+let toplam = 0
+function mukemmelSayi(nummer){
+
+}
