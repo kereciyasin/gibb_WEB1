491bbc8cd74047b2c6fe2c82678c8d6117060707
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-26 15:29:57 +0100

    datatypes

---
+++ web1-lb2/02-Variables/variables.js
@@ -0,0 +1,5 @@
+let firsName = "Yasin"
+let lastName = "Kereci"
+let age = 29
+
+console.log(firsName, lastName, age);
---
+++ web1-lb2/03-Datatypes/datatypes.js
@@ -0,0 +1,11 @@
+let isTrue = true;
+let num = 42;
+let str = "Hallo Welt";
+let undef;
+let nully = null;
+
+console.log(typeof isTrue);
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof undef);
+console.log(typeof nully);
