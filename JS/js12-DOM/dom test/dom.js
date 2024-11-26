c5d420a35d2ec256138f9700d4e0ab9ad1e429b1
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-26 11:45:33 +0100

    arrayys

--- JS/js12-DOM/dom test/dom.js
+++ JS/js12-DOM/dom test/dom.js
@@ -1,9 +1,32 @@
-function chanceButton(){
-  const btnName = document.getElementById("btnName")
-  btnName.textContent = "Gelickt!"
-  chanceTitle()
-}
-function chanceTitle(){
-  const titleName = document.getElementById("titleName")
-  titleName.textContent = "Yeni Baslik"
-}
+// function chanceButton(){
+//   const btnName = document.getElementById("btnName")
+//   btnName.textContent = "Gelickt!"
+//   chanceTitle()
+// }
+// function chanceTitle(){
+//   const titleName = document.getElementById("titleName")
+//   titleName.textContent = "Yeni Baslik"
+// }
+
+// for (let i = 0; i<10; i++){
+//   if(i % 2 == 0){
+
+//   }
+//   else{console.log(i);}
+  
+// }
+
+// let i = 1
+// let toplam = 0
+// while(i<100){
+
+//   toplam += i
+//   i++
+
+// }
+// console.log(toplam);
+
+let vehicles = ["Auto", "Fahrrad", "Motorrad"]
+vehicles[1] = "Roller"
+vehicles.push("Skateboard")
+console.log(vehicles);
