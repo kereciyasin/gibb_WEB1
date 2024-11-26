1f896cdb32a65247e3bf44ce7e20e6b5f9a73ab4
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-26 13:26:53 +0100

    objects

--- JS/js12-DOM/dom test/objects.js
+++ JS/js12-DOM/dom test/objects.js
@@ -1,12 +1,22 @@
-const car = {
-  brand: "BMW",
-  model: "x200",
-  year: 2001,
-  isElectric: true,
+// const car = {
+//   brand: "BMW",
+//   model: "x200",
+//   year: 2001,
+//   isElectric: true,
 
-  drive: function() {
-    console.log("Das Auto fährt los!");
-  }
+//   drive: function() {
+//     console.log("Das Auto fährt los!");
+//   }
  
+// }
+// car.drive()
+
+function outerFunction(arg1){
+  let variable1 = "Ben Kapaliyim";
+
+  return function innerFuction(arg2){
+    return `${arg1}, ${arg2}, ${variable1}`;
+  }
 }
-car.drive()
+const closure = outerFunction("Merhaba");
+console.log(closure("Merhaba Dünya"));
