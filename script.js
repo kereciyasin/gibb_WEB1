b313ece7c526fba91d6626d8220fd6a5ae8c5de4
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-27 21:21:43 +0100

    bereite vor

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const students = [
-  { name: "Veli", age: 22, major: "Computer Science" },
-  { name: "Musa", age: 22, major: "Computer Science" },
-  { name: "Kereci", age: 22, major: "BT" },
-  { name: "Halil", age: 22, major: "Student" },
-];
-const students1 = [
-  { name: "Alice", age: 20, major: "Mathematics" },
-  { name: "Bob", age: 22, major: "Computer Science" },
-  { name: "Ahmet", age: 22, major: "Computer Science" },
+let ogr1 = ["Yigit", "Bilge", 2010, [70, 60, 80]];
+let ogr2 = ["Ada", "Bilgi", 2012, [80, 80, 90]];
+let ogr3 = ["Ahmet", "Turan", 2008, [60, 60, 70]];
 
-  { name: "Halil", age: 22, major: "Student" },
-];
+let arr = [ogr1, ogr2, ogr3];
 
-const book = { title: "1984", author: "George Orwell" };
-const { title, author } = book;
-console.log(`${title}`);
+function yas(x) {
+  let yasHesapla = 2024 - arr[x][2];
+  return console.log(`Yas ${yasHesapla}`);
+}
+
+function ortalama(x) {
+  let toplam = 0;
+  for (let i = 0; i < arr[x][3].length; i++) {
+    toplam = toplam + arr[x][3][i];
+  }
+  let ort = toplam / arr[x][3].length;
+  return console.log(`Ortlama ${ort}`);
+}
+ortalama(0);
