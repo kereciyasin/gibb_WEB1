acdc8b97457c138456612899131327c2b0d14cc6
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-28 13:54:59 +0100

    added

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-let ogr1 = ["Yigit", "Bilge", 2010, [70, 60, 80]];
-let ogr2 = ["Ada", "Bilgi", 2012, [80, 80, 90]];
-let ogr3 = ["Ahmet", "Turan", 2008, [60, 60, 70]];
-
-let arr = [ogr1, ogr2, ogr3];
-
-function yas(x) {
-  let yasHesapla = 2024 - arr[x][2];
-  return console.log(`Yas ${yasHesapla}`);
+function changeTitle() {
+  let baslik = document.getElementById("changeTitle");
+  baslik.textContent = "Beni degistirdin";
+}
+function changeButton() {
+  let btn = document.getElementById("buttonClick");
+  btn.textContent = "Tikladin!!!";
 }
 
-function ortalama(x) {
-  let toplam = 0;
-  for (let i = 0; i < arr[x][3].length; i++) {
-    toplam = toplam + arr[x][3][i];
-  }
-  let ort = toplam / arr[x][3].length;
-  return console.log(`Ortlama ${ort}`);
-}
-ortalama(0);
+const btn1 = document.getElementById("buttonClick");
+btn1.addEventListener("click", function () {
+  changeTitle();
+});
