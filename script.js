fa28276c51f6c8764f94a95a259da904858e635d
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-28 15:28:54 +0100

    ad

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-function changeTitle() {
-  let baslik = document.getElementById("changeTitle");
-  baslik.textContent = "Beni degistirdin";
-}
-function changeButton() {
-  let btn = document.getElementById("buttonClick");
-  btn.textContent = "Tikladin!!!";
-}
+let url =
+  "https://www.udemy.com/course/modern-javascript-kursu/learn/lecture/11495746#overview";
 
-const btn1 = document.getElementById("buttonClick");
-btn1.addEventListener("click", function () {
-  changeTitle();
-});
+let kursAdi =
+  "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.";
+
+  
